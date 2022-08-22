ee21b1f778d1adf1e770360265faf855fd6968fd
amandaasmac <108792107+amandaasmac@users.noreply.github.com>
2022-08-21 23:26:42 -0300

    Imported weather app from codesandbox

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+import Weather from "./Weather";
+
+export default function App() {
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState("");
+  const [temp, setTemp] = useState("");
+  const [description, setDescription] = useState("");
+  const [maxTemp, setMaxTemp] = useState("");
+  const [minTemp, setMinTemp] = useState("");
+  const [feelsLike, setFeelsLike] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [windSpeed, setWindSpeed] = useState("");
+  const [icon, setIcon] = useState("");
+
+  function handleSearch(event) {
+    event.preventDefault();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=f2a962d48c46d7fc23aca5910b2db6af&units=metric`;
+    axios.get(apiUrl).then(showWeather);
+  }
+
+  function showWeather(response) {
+    setCity(response.data.name);
+    setTemp(Math.round(response.data.main.temp));
+    setDescription(response.data.weather[0].description);
+    setMaxTemp(Math.round(response.data.main.temp_max));
+    setMinTemp(Math.round(response.data.main.temp_min));
+    setFeelsLike(Math.round(response.data.main.feels_like));
+    setHumidity(response.data.main.humidity);
+    setWindSpeed(response.data.wind.speed);
+    setIcon(response.data.weather[0].icon);
+  }
+
+  function updateCity(event) {
+    setSearch(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-container">
+        <header>
+          <button className="unit-switch">˚F</button>
+          <form className="search-form" onSubmit={handleSearch}>
+            <input
+              type="search"
+              name="searchcity"
+              className="search-input"
+              placeholder="Search city..."
+              onChange={updateCity}
+            />
+            <input type="submit" value="Search" className="search-button" />
+          </form>
+          <button className="use-device-button">
+            <i className="fa-solid fa-location-dot"></i> Use device location
+          </button>
+        </header>
+        <Weather
+          city={city}
+          temp={temp}
+          description={description}
+          maxTemp={maxTemp}
+          minTemp={minTemp}
+          feelsLike={feelsLike}
+          humidity={humidity}
+          windSpeed={windSpeed}
+          icon={icon}
+        />
+      </div>
+      <footer>
+        <small>
+          <a href="/">Open source code</a> by Amanda
+        </small>
+      </footer>
     </div>
   );
 }
-
-export default App;
